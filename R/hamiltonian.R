97e8b823dd35e9e219951fda70c391ef4f99b1bd
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-08-21 12:22:28 +0200

    add inverse power function

--- R/hamiltonian.R
+++ R/hamiltonian.R
@@ -11,3 +11,18 @@ minimum_distance <- function(mat, min) {
 
   return(mat)
 }
+
+#' @title Inverse power function
+#' @description calculate the cost of distances based on an inverse power law
+#' @param mat a distance matrix
+#' @param power the power to be used; default 2
+#' @examples
+#' distmat <- matrix(runif(n=64),8,8)
+#' diag(distmat) <- 0
+#' inverse_power(mat = distmat, power= 2)
+#' @export
+inverse_power <- function(mat, power = 2) {
+    mat <- (1/mat)^power
+    mat[mat==Inf] <- 0
+    return(mat)
+}
