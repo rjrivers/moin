e70571673ead0dba21f2edb4fac570bd8a8482ac
Wolfgang Hamer <hamer@geographie.uni-kiel.de>
2018-08-21 12:16:27 +0200

    PPA draft implemented

--- Playground/kneighbour_functions.r
+++ Playground/kneighbour_functions.r
@@ -1,19 +1,37 @@
 
 testdata <- sf::st_read("../vignettes/data/OldenburgerGraben/Placenames.shp") 
 
-
+# MDM - Maximum Distance Modell
 D <- 5000
-output <- spatgraphs::spatgraph(sf::st_coordinates(testdata),
+mdm <- spatgraphs::spatgraph(sf::st_coordinates(testdata),
                       "geometric",
                       par=D)
 
-output2 <- igraph::graph_from_adj_list(output$edges, mode = "all")
+spatgraph_to_tidylist <- function(x,output="igraph"){
+  igraphobj <- igraph::graph_from_adj_list(x$edges, 
+                                           mode = "all")
+  
+  nbmatrix <- igraph::as_adjacency_matrix(igraphobj,sparse=FALSE)
+  
+  tidygraphobj <- tidygraph::as_tbl_graph(igraphobj,directed=FALSE)
+  
+  return(switch(output, "igraph" = igraphobj, 
+                "matrix" = nbmatrix, 
+                "tidygraph" = tidygraphobj))
+}
 
-matrixout <- igraph::as_adjacency_matrix(output2,sparse=FALSE)
+spatgraph_to_tidylist(x=mdm,output="tidygraph")
 
-output3 <- tidygraph::as_tbl_graph(output2,directed=FALSE)
+# PPA - Proximal Point Analysis
+
+k <- 5
+ppa <- spatgraphs::spatgraph(sf::st_coordinates(testdata),
+                                "knn",
+                                par=k)
+
+spatgraph_to_tidylist(x=ppa,output="tidygraph")
 
 
+  
 
 
-plot(output3)
