f997218916ead9607ac294a67a618e4a131cca73
joeroe <jroeuk@gmail.com>
2018-08-22 12:20:05 +0200

    Added ARIADNE Hamiltonian function (after Evans)

--- R/hamiltonian.R
+++ R/hamiltonian.R
@@ -5,6 +5,25 @@ hamiltonian <- function(node_data, node_formula = ~x, edges) {
   # Construct a function from ^
 }
 
+# set diagonals to 0?
+# S vector
+# v vector (random 0-1)
+# d deterrence matrix
+# e matrix (random 0-1)
+# k scalar constant
+# l scalar constant
+# j scalar constant
+# u scalar constant
+h_ariadne <- function(S, v, d, e, k, l, j, u) {
+  kappa <- sum(S * v * (1 - v))
+  lambda <- sum(matrix(S*v, 1, length(S)) %*% (d * e) %*% matrix(S*v, length(S), 1))
+  jay <- sum(S * v)
+  mu <- sum(matrix(S*v, 1, length(S)) %*% e)
+
+  H <- -(k*kappa) - (l*lambda) + (j*jay) + (u*mu)
+  return(H)
+}
+
 recursion <- function(mat, beta, hfunc, threshold) {
   H1 <- hfunc(mat)
   H2 <- H1 # Change one node
