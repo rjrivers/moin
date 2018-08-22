0bd7eaaa227c108440dc3f0741e549c1a8177c3d
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-08-22 22:06:19 +0200

    add code of function test run

--- HamiltonianTests/playground.R
+++ HamiltonianTests/playground.R
@@ -16,3 +16,16 @@ costs <- distmat %>%
 costs <- distmat %>%
     inverse_exponentional()
 
+test_d <- matrix(runif(625),25,25)
+
+hamiltonian_metrop(hfunc = h_ariadne,
+                   hvars = list(v = runif(25),
+                                e = matrix(runif(625),25,25)),
+                   hconsts = list(S = rep(1,25),
+                                  d = test_d,
+                                  k = 1,
+                                  l = 0,
+                                  j = 0,
+                                  u = 0),
+                   beta = 100,
+                   threshold = 1e-02)
