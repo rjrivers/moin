0dd740ffafd7140516c6dd9f6c4bfd8494c449dd
juhly <jbonness@dai-hzd-086.local>
2018-08-22 23:45:37 +0200

    minor syntax improvements

--- R/typecount_fromNodes.R
+++ R/typecount_fromNodes.R
@@ -1,12 +1,10 @@
 #test
-import_stuff = function(csv = "Data/shkr-weapons.csv"){
-    data = read.csv(csv, sep = ";")
-    ti_data = tibble::as_tibble(data)
+import_stuff <- function(csv = "Data/shkr-weapons.csv"){
+    data <- read.csv(csv, sep = ";")
+    ti_data <- tibble::as_tibble(data)
     return(ti_data)
 }
-
-csv2 = "Data/output_from_Script_Aggr_fea.csv"
-result_from_nodes = read.csv(csv2, sep = ",")
+result_from_nodes <- read.csv(csv2, sep = ",")
 
 ##TODO add input variable for output
 
@@ -17,17 +15,17 @@ result_from_nodes = read.csv(csv2, sep = ",")
 #'@export
 
 create.empty.typelist <- function(typelist, input){
-    nodes = unique(input[, 2])
-    data = matrix(nrow = length(nodes), ncol = length(typelist) + 1, data = 0)
-    data[, 1] = sort(unique(input[, 2]))
-    data = as.data.frame(data)
-    colnames(data) = c("node_id", typelist)
+    nodes <- unique(input[, 2])
+    data <- matrix(nrow = length(nodes), ncol = length(typelist) + 1, data = 0)
+    data[, 1] <- sort(unique(input[, 2]))
+    data <- as.data.frame(data)
+    colnames(data) <- c("node_id", typelist)
     return(data)
 }
 
-test_typelist = create.type.generator(type_tibble = import_stuff(), column = "type", pre_size = 1)
-test_export = create.empty.typelist(test_typelist, result_from_nodes)
-output = test_export
+test_typelist <- create.type.generator(type_tibble = import_stuff(), column = "type", pre_size = 1)
+test_export <- create.empty.typelist(test_typelist, result_from_nodes)
+output <- test_export
 
 ""
 #ALL ABOVE CAN GO UNLESS WE STILL NEED CREATE
@@ -40,22 +38,22 @@ output = test_export
 #'@export 
 
 create.typesectrum.for.nodes <- function(node_id, node_type, list_to_modify){
-    list_to_modify = list_to_modify
-    typelist_node = mtypes(node_type, 1)
-    indexes = c()
+    list_to_modify <- list_to_modify
+    typelist_node <- mtypes(node_type, 1)
+    indexes <- c()
     
     for (element in typelist_node) {
-        which = which(colnames(list_to_modify) == element)
-        indexes = c(indexes, which) }
+        which <- which(colnames(list_to_modify) == element)
+        indexes <- c(indexes, which) }
     
-    row = which(list_to_modify$node_id == node_id)
-    list_to_modify[row, indexes] = list_to_modify[row, indexes] + 1
+    row <- which(list_to_modify$node_id == node_id)
+    list_to_modify[row, indexes] <- list_to_modify[row, indexes] + 1
         
     return(list_to_modify)
 }
 
 #TODO decide what to do. either in other function or in wrapper
 for (i in 1:nrow(result_from_nodes)) {
-    output = create.typesectrum.for.nodes(result_from_nodes[i,2], result_from_nodes[i,1], output)
+    output <- create.typesectrum.for.nodes(result_from_nodes[i,2], result_from_nodes[i,1], output)
 }
 
