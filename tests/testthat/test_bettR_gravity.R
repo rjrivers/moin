0a2abe1984fc21e54430a21454b5445b5c4120bd
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2018-08-23 15:39:00 +0200

    written tests for deterrence function

---
+++ tests/testthat/test_bettR_gravity.R
@@ -0,0 +1,29 @@
+library(moin)
+
+context("Deterrence Function")
+
+test_that("deterrence function defaults to exponential",{
+  expect_equal(deterrence_function(matrix(c(1,1,1,1),nrow=2),2),
+               exp(-2 * matrix(c(1,1,1,1),nrow=2)))
+  })
+
+test_that("deterrence function gives correct results for different type",{
+  expect_equal(deterrence_function(matrix(c(1,1,1,1),nrow=2),2, type = "exponential"),
+               exp(-2 * matrix(c(1,1,1,1),nrow=2)))
+  expect_equal(deterrence_function(matrix(c(2,2,2,2),nrow=2),2, type = "negpower"),
+               matrix(c(2,2,2,2), nrow=2) ^-2)
+  expect_equal(deterrence_function(matrix(c(2,2,2,2),nrow=2),2, type = "power"),
+               matrix(c(2,2,2,2), nrow=2) ^2)
+})
+
+test_that("deterrence function gives an error for wrong type parameter", {
+  expect_error(
+  deterrence_function(matrix(c(1,1,1,1),nrow=2),2, type = "horst"),
+  "Sorry, I do not know this kind of deterrence function")
+})
+
+test_that("deterrence function gives no error for correct type parameter", {
+  expect_error(
+    deterrence_function(matrix(c(1,1,1,1),nrow=2),2, type = "power"),
+    NA)
+})
--- tests/testthat/test_simple.r
+++
@@ -1,7 +0,0 @@
-library(moin)
-
-context("Stupid test")
-
-test_that("1 + 2 = 3", {
-  expect_equal(1+2, 3)
-})
