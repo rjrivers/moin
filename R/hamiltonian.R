c13916a0c09016a240c1f51bf5443c4de11b3d48
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-08-23 17:55:00 +0200

    more template like stuff for the documentation

--- R/hamiltonian.R
+++ R/hamiltonian.R
@@ -16,175 +16,196 @@
 # * Capital letters are matrices (i.e. edge variables)
 # * Small letters are vectors (i.e. node variables) or universal scalar
 
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param E 
-##' @param Si 
-##' @param Sj 
-##' @return 
-##' @author 
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param E 
+#' @param Si 
+#' @param Sj 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}> 
 h_omega <- function(E, Si = 1, Sj = 1) {
   SiSj <- matrix(rep(Si, ncol(E)), nrow(E), ncol(E)) *
           matrix(rep(Sj, each = nrow(E)), nrow(E), ncol(E))
   sum(E * (log(E / SiSj) - 1))
 }
 
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param alpha 
-##' @param E 
-##' @param f 
-##' @return 
-##' @author 
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param alpha 
+#' @param E 
+#' @param f 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}> 
 h_alpha <- function(alpha, E, f) {
   alpha * (sum(E) - f)^2
 }
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param beta 
-##' @param E 
-##' @param C 
-##' @param c 
-##' @return 
-##' @author 
+
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param beta 
+#' @param E 
+#' @param C 
+#' @param c 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}> 
 h_beta <- function(beta, E, C, c) {
   beta * (sum(E*C) - c)^2
 }
 
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param gammas 
-##' @param E 
-##' @param g 
-##' @param margin 
-##' @return 
-##' @author 
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param gammas 
+#' @param E 
+#' @param g 
+#' @param margin 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}> 
 h_gamma <- function(gammas, E, g, margin) {
   sum(gammas) * (apply(E, margin, sum) - g)^2
 }
 
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param delta 
-##' @param X 
-##' @param g 
-##' @param s 
-##' @return 
-##' @author 
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param delta 
+#' @param X 
+#' @param g 
+#' @param s 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}> 
 h_delta <- function(delta, X, g, s) {
   delta * (X - sum(g * (log(g / s) - 1)))^2
 }
 
 # Gravity models ----------------------------------------------------------
 
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param E 
-##' @param Si 
-##' @param Sj 
-##' @param f 
-##' @param alpha 
-##' @param beta 
-##' @param C 
-##' @param c 
-##' @return 
-##' @author
-##' @export
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param E 
+#' @param Si 
+#' @param Sj 
+#' @param f 
+#' @param alpha 
+#' @param beta 
+#' @param C 
+#' @param c 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}>
+#' @export
 h_simple_gravity <- function(E, Si = 1, Sj = 1, f, alpha, beta, C, c) {
   h_omega(E, Si, Sj) + h_alpha(alpha, E, f) + h_beta(beta, E, C, c)
 }
 
 
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param E 
-##' @param Si 
-##' @param Sj 
-##' @param beta 
-##' @param C 
-##' @param c 
-##' @param gammas 
-##' @param g 
-##' @param margin 
-##' @return 
-##' @author
-##' @export
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param E 
+#' @param Si 
+#' @param Sj 
+#' @param beta 
+#' @param C 
+#' @param c 
+#' @param gammas 
+#' @param g 
+#' @param margin 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}>
+#' @export
 h_constrained_gravity <- function(E, Si = 1, Sj = 1, beta, C, c, gammas, g, margin = 1) {
  h_omega(E, Si, Sj) + h_beta(beta, E, C, c) + h_gamma(gammas, E, g, margin)
 }
 
 # Doubly constrained model need to check that sum of Is = sum of Os
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param E 
-##' @param Si 
-##' @param Sj 
-##' @param beta 
-##' @param C 
-##' @param c 
-##' @param in_gammas 
-##' @param in_g 
-##' @param out_gammas 
-##' @param out_g 
-##' @return 
-##' @author 
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param E 
+#' @param Si 
+#' @param Sj 
+#' @param beta 
+#' @param C 
+#' @param c 
+#' @param in_gammas 
+#' @param in_g 
+#' @param out_gammas 
+#' @param out_g 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}> 
 h_double_constrained_gravity <- function(E, Si = 1, Sj = 1, beta, C, c, in_gammas, in_g, out_gammas, out_g) {
  h_omega(E, Si, Sj) + h_beta(beta, E, C, c) + h_gamma(in_gammas, E, in_g, margin = 1) + h_gamma(out_gammas, E, out_g, margin = 2)
 }
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param E 
-##' @param Si 
-##' @param Sj 
-##' @param beta 
-##' @param C 
-##' @param c 
-##' @param gammas 
-##' @param g 
-##' @param delta 
-##' @param X 
-##' @param s 
-##' @return 
-##' @author
-##' @export
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param E 
+#' @param Si 
+#' @param Sj 
+#' @param beta 
+#' @param C 
+#' @param c 
+#' @param gammas 
+#' @param g 
+#' @param delta 
+#' @param X 
+#' @param s 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}>
+#' @export
 h_retail <- function(E, Si = 1, Sj = 1, beta, C, c, gammas, g, delta, X, s) {
  h_omega(E, Si, Sj) + h_beta(beta, E, C, c) + h_gamma(gammas, E, g, margin = 1) + h_delta(delta, X, g, s)
 }
-##' .. content for \description{} (no empty lines) ..
-##'
-##' .. content for \details{} ..
-##' @title 
-##' @param E 
-##' @param Si 
-##' @param Sj 
-##' @param f 
-##' @param in_delta 
-##' @param out_delta 
-##' @param in_g 
-##' @param in_s 
-##' @param out_g 
-##' @param out_s 
-##' @return 
-##' @author
-##' @export
+#' .. content for \description{} (no empty lines) ..
+#'
+#' .. content for \details{} ..
+#' @title 
+#' @param E 
+#' @param Si 
+#' @param Sj 
+#' @param f 
+#' @param in_delta 
+#' @param out_delta 
+#' @param in_g 
+#' @param in_s 
+#' @param out_g 
+#' @param out_s 
+#' @return 
+#' @author Daniel Knitter <\email{knitter@@geographie.uni-kiel.de}>
+#' @author Joe Roe <\email{jwg983@@hum.ku.dk}>
+#' @author Ray Rivers <\email{r.rivers@@imperial.ac.uk}>
+#' @export
 h_alonso <- function(E, Si = 1, Sj = 1, f, in_delta, out_delta, in_g, in_s = rep(1, length(in_g)), out_g, out_s = rep(1, length(out_g))) {
  h_omega(E, Si, Sj) + h_alpha(alpha, E, f) + h_delta(in_delta, X, in_g, in_s) + h_delta(out_delta, X, out_g, out_s)
 }
