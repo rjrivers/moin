3b4dc03bae887671616539cddc5066adc01420d0
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-08-23 07:30:32 +0200

    add medieval settlements as data object

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Medieval Settlements in Wagrien, Schleswig-Holstein, Germany
+#'
+#' Data digitized from topographic maps
+#'
+#' @format A data.frame
+#' \describe{
+#'   \item{Name}{The name of the site as written on the map}
+#'   \item{rank}{The rank of the site as written on the map}
+#'   \item{x}{The x-coordiante in EPSG:25832 as dervied from the georeferenced point}
+#'   \item{y}{The y-coordiante in EPSG:25832 as dervied from the georeferenced point}
+#' @source TODO:add reference
+"medieval_settlements"
