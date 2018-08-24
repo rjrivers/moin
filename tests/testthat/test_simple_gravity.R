2b02778ccfac13286f403b400affe318d7a0d34d
Martin Hinz <martin.hinz@ufg.uni-kiel.de>
2018-08-24 10:06:39 +0200

    implemented test against ground truth of Thomas&Hugget 1980
    Also fixed test of simple gravity function according to code change of f82a8fc2792e2f319f5e0b15048f09573c9e1f8f

--- tests/testthat/test_simple_gravity.R
+++ tests/testthat/test_simple_gravity.R
@@ -10,7 +10,6 @@ test_that ("Calculating Tij from friction constant k, matrix of mass and
 deterence function on cost matrix", {
   expect_equal ({
     Mmatrix <- Mi%o%Mi
-    diag(Mmatrix)<- 0
     k*((Mmatrix)/fcij)},
     simple_gravity(Mi, fcij, k)
   )
@@ -23,4 +22,15 @@ test_that ("Output is matrix", {
   
   expect_is(Tij, "matrix")
   
+})
+
+# Test against 'ground truth' from Thomas&Hugget 1980, 138
+test_that ("tij matrix from a simple gravity model", {
+  expect_equal({
+    round(simple_gravity(c(100, 50, 150),
+                   deterrence_function(
+                     matrix(c(7.5,30,50,30,7.5,40,50,40,10), nrow=3), 2, type="power")
+    ), 1)},
+    matrix(c(177.8, 5.6, 6, 5.6, 44.4, 4.7, 6, 4.7, 225), nrow=3)
+  )
 })
