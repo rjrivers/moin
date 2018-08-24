81dbfcf0b92f03c9a6299e4b9f442706a3717a70
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-08-24 08:34:26 +0200

    start "standardized" test application of functions

--- HamiltonianTests/playground.R
+++ HamiltonianTests/playground.R
@@ -1,47 +1,47 @@
+## Example Applications
 
-raw <- sf::read_sf("Placenames.shp")
-sites <- raw %>%
-    `[`(1:10,)
+## Data setup
+data(medieval_settlements, package = "moin")
 
-distmat <- sf::st_distance(sites)
-
-distmat <- sites %>%
-    as("Spatial") %>%
-    rgeos::gDistance(byid = TRUE) %>%
-    `/`(1000)
-
-costs <- distmat %>%
-    inverse_power()
+distmat <- medieval_settlements %>%
+ `[`(1:5,) %>%  
+  sf::st_as_sf(coords = c("x","y"),
+               crs = 25832) %>%  
+  as("Spatial") %>%
+  rgeos::gDistance(byid = TRUE) %>%
+  `/`(1000)
 
 costs <- distmat %>%
-    inverse_exponentional()
+  moin::inverse_exponentional()
 
-test_d <- matrix(runif(625),25,25)
+test_d <- matrix(runif(length(costs)),ncol(costs),nrow(costs))
 
-res <- hamiltonian_metrop(hfunc = h_ariadne,
-                   hvars = list(v = runif(10),
-                                e = matrix(runif(100),10,10)),
-                   hconsts = list(S = rep(1,10),
-                                  d = costs,
-                                  k = 1,
-                                  l = 1,
-                                  j = 1,
-                                  u = 1),
-                   beta = 100,
-                   threshold = 1e-01)
-res
+## H-Ariadne
+res <- hamiltonian_metrop(
+  hfunc = h_ariadne,
+  hvars = list(v = runif(ncol(costs)),
+               e = test_d),
+  hconsts = list(S = rep(1,ncol(costs)),
+                 d = costs,
+                 k = 1,
+                 l = 1,
+                 j = 1,
+                 u = 1),
+  hvar_constraints = list(v = c(0,1),
+                          e = c(0,1)),
+  beta = 100,
+  threshold = 1e-01)
 
-# TODO: "e" in the function should be created automatically based on the size of d
-
-test_d <- matrix(runif(25),5,5)
-
+## h_simple_gravity
 res <- hamiltonian_metrop(hfunc = h_simple_gravity,
-                   hvars = list(E = test_d),
-                   hconsts = list(c = 4,
-                                  f = 2,
-                                  C = costs,
-                                  alpha = 64,
-                                  beta = 64),
-                   hvar_constraints = list(E = c(0,1)),
-                   beta = 100,
-                   threshold = 1)
+                          hvars = list(E = test_d),
+                          hconsts = list(c = 4,
+                                         f = 2,
+                                         C = costs,
+                                         alpha = 64,
+                                         beta = 64),
+                          hvar_constraints = list(E = c(0,1)),
+                          beta = 100,
+                          threshold = 1)
+
+
