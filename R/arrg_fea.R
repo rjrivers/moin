663c5bbb2b786f175f89015285ab2df76d83640c
Franziska Faupel <ffaupel@ufg.uni-kiel.de>
2018-08-24 09:41:35 +0200

    Update arrg_fea.R
    editing documentation

--- R/arrg_fea.R
+++ R/arrg_fea.R
@@ -1,6 +1,9 @@
-#' Aggregate features by Voronoi tesselation
+#' Aggregate Features by Voronoi Tesselation
+#'
 #' Creates a Voronoi tesselation of the nodes and aggregates features in tiles according to their spatial location. Returns a dataframe of aggregated features and their associated node as a new collum.
+#' 
 #' @title aggr_fea_voro
+#'
 #' @param nodes a data.frame containing metric x and y coordinates of nodes, additionally an identifier. Coordinates are expected to be the first two columns.
 #' @param features a data.frame containing metric x and y coordinates of nodes, and feature type. Coordinates are expected to be the first two columns.
 #' @param type_col a character string naming the columname containing feature types.
@@ -8,12 +11,15 @@
 #' @return a dataframe with feature types and their corresponding node. Additionally a
 #'     plot is created to display the aggregation of features to nodes. Amount of feature
 #'     per node is added as number to the plot.
-#' @export
+#'
+#'
 #' @author Oliver Nakoinz <\email{oliver.nakoinz@@ufg.uni-kiel.de}>
 #' @author Chiara Girotto <\email{chiara.girotto@@web.de}>
 #' @author Franziska Faupel <\email{franziska-faupel@@gmx.de}>
+#'
 #' @examples
 #' 
+#' @export
 
 aggr_fea_voro <- function(nodes, features, type_col){
     ## Creating a global window to ensure all points will be included. Minimises/enlarges the window by +/- 1 to avoid exclusion of points at the border
@@ -31,13 +37,15 @@ aggr_fea_voro <- function(nodes, features, type_col){
     return(df_aggr_fea)
 }
 
-#' Create global bounding box
+#' Create Global Bounding Box
 #' 
 #' @title global_bb
+#' 
 #' @param nodes a data.frame containing metric x and y coordinates of nodes, additionally an identifier. Coordinates are expected to be the first two columns.
 #' @param features a data.frame containing metric x and y coordinates of nodes, and feature type. Coordinates are expected to be the first two columns.
 #'
 #' @return an owin object which is slightly bigger, to avoid missing points due to edge effects
+#' 
 #' @author Oliver Nakoinz <\email{oliver.nakoinz@@ufg.uni-kiel.de}>
 #' @author Chiara Girotto <\email{chiara.girotto@@web.de}>
 #' @author Franziska Faupel <\email{franziska-faupel@@gmx.de}>
@@ -59,11 +67,13 @@ global_bb <- function(nodes, features){
 #' Plotting Voronoi Tesselation with Amount of Features
 #' 
 #' @title plot_voro_fea
+#' 
 #' @param nodes a data.frame containing metric x and y coordinates of nodes, additionally an identifier. Coordinates are expected to be the first two columns.
 #' @param df_aggr_fea a dataframe with feature types and their corresponding node
 #' @param voronoi a tesselation list object generated from nodes
 #'
 #' @return an owin object which is slightly bigger, to avoid missing points due to edge effects
+#' 
 #' @author Juliane Watson <\email{juliane.bonness@@web.de}>
 
 plot_voro_fea <- function(df_aggr_fea, nodes, voronoi){
