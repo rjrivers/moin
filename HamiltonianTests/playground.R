625bfc89f87bfa416875353a677c6383f6f59ceb
Daniel Knitter <knitter@geographie.uni-kiel.de>
2018-08-24 09:01:19 +0200

    add try out runs; problems with the gammas...!?

--- HamiltonianTests/playground.R
+++ HamiltonianTests/playground.R
@@ -20,28 +20,72 @@ test_d <- matrix(runif(length(costs)),ncol(costs),nrow(costs))
 res <- hamiltonian_metrop(
   hfunc = h_ariadne,
   hvars = list(v = runif(ncol(costs)),
-               e = test_d),
-  hconsts = list(S = rep(1,ncol(costs)),
-                 d = costs,
+               E = test_d),
+  hconsts = list(s = rep(1,ncol(costs)),
+                 D = costs,
                  k = 1,
                  l = 1,
                  j = 1,
                  u = 1),
   hvar_constraints = list(v = c(0,1),
-                          e = c(0,1)),
+                          E = c(0,1)),
   beta = 100,
   threshold = 1e-01)
 
-## h_simple_gravity
-res <- hamiltonian_metrop(hfunc = h_simple_gravity,
+## Alonso
+res <- hamiltonian_metrop(hfunc = h_alonso,
                           hvars = list(E = test_d),
-                          hconsts = list(c = 4,
-                                         f = 2,
-                                         C = costs,
+                          hconsts = list(f = 1,
+                                         in_g = rep(.1,ncol(costs)),
+                                         out_g = rep(.1,ncol(costs)),
                                          alpha = 64,
-                                         beta = 64),
+                                         X = 2,
+                                         in_delta = 2,
+                                         out_delta = 3),
                           hvar_constraints = list(E = c(0,1)),
                           beta = 100,
                           threshold = 1)
 
+##################################################
+##################################################
+########## PROBLEMS ####################
+res <- hamiltonian_metrop(hfunc = h_constrained_gravity,
+                          hvars = list(E = test_d),
+                          hconsts = list(c = 4,
+                                         gammas = rep(.1,ncol(costs)),
+                                         g = .1,
+                                         C = costs,
+                                         beta = 64),
+                          hvar_constraints = list(E = c(0,1)),
+                          beta = 100,
+                          threshold = 1)
+warnings()
 
+res <- hamiltonian_metrop(hfunc = h_double_constrained_gravity,
+                          hvars = list(E = test_d),
+                          hconsts = list(c = 4,
+                                         in_gammas = rep(.1,ncol(costs)),
+                                         in_g = .1,
+                                         out_gammas = rep(.1,ncol(costs)),
+                                         out_g = .1,
+                                         C = distmat,
+                                         beta = 64),
+                          hvar_constraints = list(E = c(0,1)),
+                          beta = 100,
+                          threshold = 50)
+warnings()
+
+res <- hamiltonian_metrop(hfunc = h_retail,
+                          hvars = list(E = test_d),
+                          hconsts = list(c = 4,
+                                         s = rep(.1,ncol(costs)),
+                                         gammas = rep(.1,ncol(costs)),
+                                         g = .1,
+                                         C = costs,
+                                         delta = 2,
+                                         X = 3,
+                                         beta = 64),
+                          hvar_constraints = list(E = c(0,1)),
+                          beta = 100,
+                          threshold = 1)
+warnings()
