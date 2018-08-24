7d3b8f1af6dc4327fa84480dad42a6e69518fc42
Wolfgang Hamer <hamer@geographie.uni-kiel.de>
2018-08-24 08:49:11 +0200

    Renamed modelgraph2 to moin_network

---
+++ R/moin_network.R
@@ -0,0 +1,55 @@
+
+#' moin_network
+#'
+#' 
+#' Generates model graphs for MDM, PPA or IOM models.
+#'
+#' @param sfobj a sf object containing point coordinates 
+#' @param method type of model graph to be produced, either a Maximum Distance Modell ("mdm"), a Proximal Point Analysis ("ppa") or a full graph as base for a Intervening Opportunity Model ("iom"); defaults to "iom" i.e. a full graph.
+#' 
+#' @param par for type "mdm" the threshold value D, and for "ppa" neighbour rank k
+#'
+#' @return a list with
+#' \itemize{
+#' \item{"graph"}{ = a graph object of class tidygraph }
+#' \item{"distance_matrix"}{ = a matrix of all interpoint distances (given in meter)}
+#' \item{"neighbourhood_matrix"}{ = an adjacency (or neighbourhood) matrix, where 0 = not connected nodes and 1 = connected nodes.}
+#' \item{"truncated_distance_matrix"}{ = a matrix, where cell(ij) = distance(ij) if nodes are connected else 0.}
+#' \item{"input_data"}{ = the sf object supplied as input.}
+#' }
+#' @export
+#' 
+#' @author Hendrik Raese <h.raese@ufg.uni-kiel.de>
+#' @author Georg Roth <georg.roth@fu-berlin.de>
+#' @author Wolfgang Hamer <hamer@geographie.uni-kiel.de>
+#' 
+#' @examples
+#' set.seed(1985)
+#' d <- data.frame(matrix(runif(15), ncol = 3))
+#' p <- sf::st_as_sf(x = d, coords = 1:2)
+#' result <- moin_network(sfobj = p, method = "mdm", par = 0.3)
+#' plot(result$graph,sf::st_coordinates(result$input_data))
+
+moin_network <- function(sfobj, method="iom", par=NULL, mode="undirected"){
+  dista <- sf::st_distance(x = sfobj, 
+                           y = sfobj)
+  
+  dista2 <- matrix(dista,nrow=nrow(dista))
+  
+  nb_mat <- switch(method, 
+                   "iom"=1*(dista2>0),
+                   "mdm"=(1*(dista2>0))*(1*(dista2 <= par)),
+                   "ppa"=apply(dista2, 2,FUN = function(x){( order(order(x))<=(par+1)& order(order(x))>1)*1}))
+  
+  graph <- igraph::graph_from_adjacency_matrix(nb_mat, mode=mode)
+  
+  tidygraphobj <- tidygraph::as_tbl_graph(graph)
+  
+  truncated <- nb_mat * dista2
+  
+  return(list(graph=tidygraphobj, 
+              distance_matrix=dista2, 
+              neighbourhood_matrix= nb_mat, 
+              truncated_distance_matrix=truncated, 
+              input_data=sfobj))
+}
